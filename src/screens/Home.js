649f34af3fcdda006c269fe4222628274dcdd352
doomsower <K.Kuznetcov@gmail.com>
2016-05-15 12:24:23 +0300

    Blank home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,66 +1,38 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet, Image} from 'react-native';
-import Button from 'apsl-react-native-button'
-// import BluetoothShutter from '../native/BluetoothShutter';
-import CaesarLogo from '../components/CaesarLogo';
 import commonStyles from '../styles/common';
+import WorkScreen from './WorkScreen';
 
 export default class Home extends Component {
-  state = {
-    pressed: false
-  };
-
-  //[{"id":"58:71:33:80:05:E3","address":"58:71:33:80:05:E3","name":"RSB-101"}]
-  componentDidMount() {
-    // BluetoothShutter.init();
-    // BluetoothShutter.listenShutter(() => {
-    //   console.log('Shutter pressed!!');
-    //   this.setState({pressed: true});
-    // });
-  }
 
   render() {
-    const content = this.state.pressed ? this.renderPressed() : this.renderSimple();
+    const righty = {
+      title: 'Настройки',
+      action: () => console.log('Righty!')
+    };
     return (
-      <View style={styles.container}>
-        {content}
-      </View>
+      <WorkScreen righty={righty}>
+        <Text style={[commonStyles.text, styles.label]}>
+          {'Скоро тут появятся\n сообщения от вашего\n питомца.'}
+        </Text>
+        <View style={{backgroundColor: 'red'}}>
+          <Image key='dog' source={require('../assets/dog.jpg')}/>
+        </View>
+        <Text style={[commonStyles.text, styles.label]}>
+          {'...а пока можно перейти\n' +
+          'в Настройки, чтобы\n' +
+          'указать его кличку\n' +
+          'и добавить фотографию.'}
+        </Text>
+      </WorkScreen>
     );
-  }
-
-  renderSimple = () => {
-    return [
-      <CaesarLogo/>,
-      <Text style={styles.label}>Ваш питомец не скучает!</Text>
-    ];
-  };
-
-  renderPressed = () => {
-    return [
-      <CaesarLogo key="toplogo"/>,
-      <Image key='dog' source={require('../assets/dog.jpg')}/>,
-      <Text key="txt" style={styles.label}>Ваш питомец соскучился!</Text>,
-      <Button key="btn" onPress={this.onCancel} style={commonStyles.button}>OK</Button>
-    ]
-  };
-
-  onCancel = () => {
-    console.log('Cancel');
-    this.setState({pressed: false});
   }
 
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    padding: 20
-  },
   label: {
-    fontSize: 24,
-    color: 'white'
+    textAlign: 'center',
+    margin: 16
   }
 });
