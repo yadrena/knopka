b839bf4f53cea380de2048aa30513b36784ae50f
doomsower <K.Kuznetcov@gmail.com>
2016-05-15 12:09:30 +0300

    Network picker

--- src/screens/ConnectMat.js
+++ src/screens/ConnectMat.js
@@ -1,20 +1,40 @@
 import React, {PropTypes} from 'react'
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, Picker, StyleSheet} from 'react-native';
 import {connect} from 'react-redux';
 import Button from 'apsl-react-native-button';
 import commonStyle, {buttonStyle} from '../styles/common';
 import {Actions} from 'react-native-router-flux';
 import WorkScreen from './WorkScreen';
+import BusyIndicator from '../components/BusyIndicator';
 
 class ConnectMat extends React.Component {
-  static propTypes = {};
+  static propTypes = {
+    wifiLoaded: PropTypes.bool,
+    wifiList: PropTypes.array
+  };
 
-  static mapStateToProps = state => ({});
+  static mapStateToProps = state => ({
+    wifiLoaded: state.wifis.loaded,
+    wifiList: state.wifis.list.map( v => v.SSID)
+  });
+
+  state={
+    wifi: null
+  };
 
   render() {
+    const {wifiLoaded, wifiList} = this.props;
+    const wifis = wifiLoaded ? wifiList.map(ssid => <Picker.Item key={ssid} label={ssid} value={ssid}/>) : null;
     return (
       <WorkScreen>
-        <Text style={commonStyle.text}>Networks...</Text>
+        <Text style={commonStyle.text}>Choose network</Text>
+        {!wifiLoaded && <BusyIndicator message="Scanning wifi networks..."/>}
+        {wifiLoaded &&
+        <Picker selectedValue={this.state.wifi} prompt="Choose network" mode="dialog"
+                onValueChange={wifi => this.setState({wifi})} style={styles.picker}>
+          {wifis}
+        </Picker>
+        }
         <Button onPress={Actions.home} {...buttonStyle}>Connect</Button>
       </WorkScreen>
     );
@@ -22,3 +42,10 @@ class ConnectMat extends React.Component {
 }
 
 export default connect(ConnectMat.mapStateToProps)(ConnectMat);
+
+var styles = StyleSheet.create({
+  picker: {
+    width: 300,
+    color: 'white'
+  }
+});
